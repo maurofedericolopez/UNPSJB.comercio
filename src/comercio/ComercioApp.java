4004e82dffddcc7ced8c2255d908a61ccf0d9157
Mauro <Mauro@Mauro>
2012-11-01 10:33:23 -0300

    modifique comercio

---
+++ src/comercio/ComercioApp.java
@@ -0,0 +1,15 @@
+package comercio;
+
+/**
+ *
+ * @author Mauro Federico Lopez
+ */
+public class ComercioApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/comercio/modelo/Producto.java
@@ -0,0 +1,53 @@
+package comercio.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Mauro Federico Lopez
+ */
+@Entity
+public class Producto implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProducto;
+
+    public Long getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Long idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idProducto != null ? idProducto.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Producto)) {
+            return false;
+        }
+        Producto other = (Producto) object;
+        if ((this.idProducto == null && other.idProducto != null) || (this.idProducto != null && !this.idProducto.equals(other.idProducto))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "comercio.modelo.Producto[ id=" + idProducto + " ]";
+    }
+    
+}
